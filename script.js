22092409f7429010fe860e1c53b49bde25115db8
GillianLEVERT <gillian.levert@gmail.com>
2023-01-19 15:09:21 +0100

    middle commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const elForm = document.querySelector(".form");
 const elInput = document.querySelector(".input");
 const list = document.querySelector(".list");
+const elSelect = document.querySelector(".select");
 
 
 
-function getUrl(inputvalue) {
+function getUrl(inputvalue, selectvalue) {
     list.innerHTML = "";
-    fetch(`http://www.omdbapi.com/?s=${inputvalue}&apikey=${OMDB_KEY}`) 
+    fetch(`http://www.omdbapi.com/?s=${inputvalue}&type=${selectvalue}&apikey=${OMDB_KEY}`) 
         .then(response => response.json())
         .then(data => {
             renderMovies(data.Search)
@@ -18,11 +19,23 @@ function renderMovies(arr) {
         const item = document.createElement("li");
         const text = document.createElement("h3");
         const img = document.createElement("img");
+        const year = document.createElement("p");
+        const plot = document.createElement("p");
+        const readMoreBtn = document.createElement("button");
         item.classList.add("item", "mb-3", "text-center", "text-bg-dark");
         img.classList.add("img", "mb-3");
+        plot.classList.add("hidden")
+        
         img.src = element.Poster;
         text.textContent = element.Title;
-        item.append(img, text);
+        year.textContent = element.Year
+        plot.textContent = element.Plot
+        
+        readMoreBtn.textContent = "Read More"
+        readMoreBtn.addEventListener("click", () => {
+            plot.classList.remove("hidden")
+        });
+        item.append(img, text, year, readMoreBtn, plot);
         list.appendChild(item);
     }))
 }
@@ -31,6 +44,6 @@ elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
 
     let elInputValue = elInput.value;
- 
-    getUrl(elInputValue, )
+    let elSelectValue = elSelect.value;
+    getUrl(elInputValue, elSelectValue )
 })
